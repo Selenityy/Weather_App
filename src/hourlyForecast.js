72127f3943c12b2e079e6b0c569820a42aaa65e2
Selena <selena.k.rodriguez@gmail.com>
2023-05-02 13:48:35 -0500

    created the function to create the html elements for an hourly forecast

---
+++ src/hourlyForecast.js
@@ -0,0 +1,29 @@
+import { createNewDiv, setClassAttr } from "./DOMlogic";
+
+// Create Individual Hours
+const individualHourNodes = () => {
+  let parentDiv = document.getElementById("hourlyGraph");
+  for (let i = 0; i <= 23; i++) {
+    let hourlyDiv = document.createElement("div");
+    hourlyDiv.setAttribute("id", "hour" + i);
+
+    let iconDiv = document.createElement("div");
+    iconDiv.setAttribute("id", "hr" + i + "icon");
+
+    let tempDiv = document.createElement("div");
+    tempDiv.setAttribute("id", "hr" + i + "temp");
+
+    hourlyDiv.appendChild(iconDiv);
+    hourlyDiv.appendChild(tempDiv);
+    parentDiv.appendChild(hourlyDiv);
+  }
+};
+
+const createHourlyForecast = () => {
+  console.log("bunny");
+  createNewDiv("hourlyGraph", "content");
+
+  individualHourNodes();
+};
+
+export { createHourlyForecast };
