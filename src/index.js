ac645b361d729e5a4ca84a46dc3b1c8e792f3564
Selena <selena.k.rodriguez@gmail.com>
2023-05-04 13:56:05 -0500

    made the api url a global variable

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import { createSearchHeader } from "./searchBar";
 import { createMainContent } from "./mainContent";
 import { createHourlyForecastFooter } from "./hourlyForecast";
 
+const baseUrl = "http://api.weatherapi.com/v1";
+
 createSearchHeader();
 
 createMainContent();
 
 createHourlyForecastFooter();
+
+export { baseUrl };
