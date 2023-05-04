23d5a32d26df0b14ac452ca1bf0f27f19c4640c9
Selena <selena.k.rodriguez@gmail.com>
2023-05-04 13:55:54 -0500

    implemented the default data and api request

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,4 +1,26 @@
+import { defaultWeather } from "./APILogic";
 import { createNewDiv, setClassAttr } from "./DOMlogic";
+
+const defaultData = () => {
+  // Current Date
+  let dateDiv = document.getElementById("currentDate");
+  let date = new Date();
+  let apiDate = date.toISOString().slice(0, 10);
+  let options = { month: "long", day: "numeric", year: "numeric" };
+  let formattedDate = date.toLocaleDateString("en-US", options);
+  dateDiv.innerHTML = formattedDate;
+
+  //Current Weather
+  let tempDiv = document.getElementById("currentTemp");
+  let defaultLocation = "Minneapolis, MN";
+  let endpoint = "/current.json";
+
+  //API Call
+  defaultWeather(endpoint, apiDate, defaultLocation).then((data) => {
+    console.log(data);
+    tempDiv.innerHTML = data.current.temp_f;
+  });
+};
 
 const createMainContent = () => {
   console.log("dog");
@@ -21,6 +43,8 @@ const createMainContent = () => {
   createNewDiv("wind", "details");
   createNewDiv("sunrise", "details");
   createNewDiv("sunset", "details");
+
+  defaultData();
 };
 
 export { createMainContent };
