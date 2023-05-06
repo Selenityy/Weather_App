166a887a214e26ebcd2b97d25b8cb220c5eba4e9
Selena <selena.k.rodriguez@gmail.com>
2023-05-06 11:46:08 -0500

    added a add text function

--- src/DOMlogic.js
+++ src/DOMlogic.js
@@ -12,4 +12,19 @@ const setClassAttr = (divId, classList) => {
   div.classList.add(classList);
 };
 
-export { createNewDiv, setClassAttr };
+// Create an Img div
+const createImg = (newImgId, parentDivId) => {
+  let parentDiv = document.getElementById(parentDivId);
+  let newImg = document.createElement("img");
+  newImg.setAttribute("id", newImgId);
+  //   newImg.src = imageName;
+  parentDiv.appendChild(newImg);
+};
+
+// Add Text to a Div
+const addText = (divId, desiredText) => {
+  let div = document.getElementById(divId);
+  div.textContent = desiredText;
+};
+
+export { createNewDiv, setClassAttr, createImg, addText };
