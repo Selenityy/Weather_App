f621828e0d2bd64d46aba2ed64df4765c4eed0d6
Selena <selena.k.rodriguez@gmail.com>
2023-05-06 10:57:16 -0500

    Began the API request for the hourly data

--- src/hourlyForecast.js
+++ src/hourlyForecast.js
@@ -1,4 +1,17 @@
 import { createNewDiv, setClassAttr } from "./DOMlogic";
+import { defaultWeather } from "./APILogic";
+
+//API Requests
+const defaultHourlyData = () => {
+  let defaultLocation = "Minneapolis, MN";
+  let forecastEndpoint = "	/forecast.json";
+  let date = new Date();
+  let apiDate = date.toISOString().slice(0, 10);
+  let h0 = document.getElementById("hour0");
+  defaultWeather(forecastEndpoint, apiDate, defaultLocation).then((data) => {
+    h0.innerHTML = data.forecast.forecastday[0].hour[0].temp_f;
+  });
+};
 
 // Create Individual Hours
 const individualHourNodes = () => {
@@ -24,6 +37,8 @@ const createHourlyForecastFooter = () => {
   createNewDiv("hourlyGraph", "content");
 
   individualHourNodes();
+
+  defaultHourlyData();
 };
 
 export { createHourlyForecastFooter };
