b6de9a114d1559b26f3c44cc82a1a4bf843417e2
Selena <selena.k.rodriguez@gmail.com>
2023-05-06 12:59:05 -0500

    cleaned up how to set hour innerhtml to be the time

--- src/hourlyForecast.js
+++ src/hourlyForecast.js
@@ -2,19 +2,18 @@ import { createNewDiv } from "./DOMlogic";
 import { defaultWeather } from "./APILogic";
 import { WEATHER_ICONS } from "./mainContent";
 
-//API Requests
+//API Requests for Temp and Icon
 const defaultHourlyData = () => {
   let defaultLocation = "Minneapolis, MN";
   let forecastEndpoint = "	/forecast.json";
   let date = new Date();
   let apiDate = date.toISOString().slice(0, 10);
-  //   let h0Temp = document.getElementById("hr0temp");
-  //   let h0Icon = document.getElementById("hr0icon");
   defaultWeather(forecastEndpoint, apiDate, defaultLocation).then((data) => {
     for (let i = 0; i <= 23; i++) {
       let temp = document.getElementById(`hr${i}temp`);
       let icon = document.getElementById(`hr${i}icon`);
-      temp.innerHTML = data.forecast.forecastday[0].hour[i].temp_f;
+      temp.innerHTML =
+        Math.round(data.forecast.forecastday[0].hour[i].temp_f) + "°" + "F";
       const iconPath = data.forecast.forecastday[0].hour[
         i
       ].condition.icon.replace("//cdn.weatherapi.com/weather/64x64/", "");
@@ -24,28 +23,20 @@ const defaultHourlyData = () => {
         }
       });
     }
-    // h0Temp.innerHTML = data.forecast.forecastday[0].hour[0].temp_f;
-    // const iconPath =
-    //   data.forecast.forecastday[0].hour[0].condition.icon.replace(
-    //     "//cdn.weatherapi.com/weather/64x64/",
-    //     ""
-    //   );
-    // WEATHER_ICONS.keys().forEach((filePath) => {
-    //   if (filePath.includes(iconPath)) {
-    //     h0Icon.src = `assets/weather_icons/${filePath}`;
-    //   }
-    // });
   });
 };
 
-// Create Individual Hours
+// Create Individual Hours Nodes
 const individualHourNodes = () => {
   let parentDiv = document.getElementById("hourlyGraph");
   for (let i = 0; i <= 23; i++) {
     let hourlyDiv = document.createElement("div");
     hourlyDiv.setAttribute("id", "hour" + i);
     hourlyDiv.setAttribute("class", "hourlyNode");
-    hourlyDiv.innerHTML = i;
+    let AmOrPm = i >= 12 ? "PM" : "AM";
+    let hours = i % 12 || 12;
+    let finalTime = hours + " " + AmOrPm;
+    hourlyDiv.innerHTML = finalTime;
 
     let iconDiv = document.createElement("img");
     iconDiv.setAttribute("id", "hr" + i + "icon");
@@ -60,7 +51,6 @@ const individualHourNodes = () => {
 };
 
 const createHourlyForecastFooter = () => {
-  console.log("bunny");
   createNewDiv("hourlyGraph", "content");
 
   individualHourNodes();
