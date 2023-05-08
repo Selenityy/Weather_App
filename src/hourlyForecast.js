7c622d9da33c905efaed3256a49f3a4704a2da4d
Selena <selena.k.rodriguez@gmail.com>
2023-05-08 18:02:41 -0500

    changed the update hourly weather to the today function

--- src/hourlyForecast.js
+++ src/hourlyForecast.js
@@ -1,40 +1,48 @@
 import { createNewDiv } from "./DOMlogic";
-import { findWeather, updateHourlyWeather } from "./APILogic";
-import { WEATHER_ICONS } from "./mainContent";
-import { forecastEndpoint, apiDate } from "./index";
-
-let today = new Date();
-let currentHour = today.getHours();
+import { findWeather, updateWeather, updateHourlyWeatherToday } from "./APILogic";
+import { forecastEndpoint, today, todayHour } from "./index";
 
 //API Requests for Temp and Icon
 const defaultHourlyData = () => {
   let defaultLocation = "Minneapolis, MN";
-  findWeather(forecastEndpoint, apiDate, defaultLocation).then((data) => {
-    updateHourlyWeather(data);
+  findWeather(forecastEndpoint, today, defaultLocation).then((data) => {
+    updateHourlyWeatherToday(data);
   });
 };
 
-// const updateHourlyWeather = (data) => {
-//   for (let i = currentHour; i <= 23; i++) {
-//     let temp = document.getElementById(`hr${i}temp`);
-//     let icon = document.getElementById(`hr${i}icon`);
-//     temp.innerHTML =
-//       Math.round(data.forecast.forecastday[0].hour[i].temp_f) + "°" + "F";
-//     const iconPath = data.forecast.forecastday[0].hour[
-//       i
-//     ].condition.icon.replace("//cdn.weatherapi.com/weather/64x64/", "");
-//     WEATHER_ICONS.keys().forEach((filePath) => {
-//       if (filePath.includes(iconPath)) {
-//         icon.src = `assets/weather_icons/${filePath}`;
-//       }
-//     });
-//   }
-// };
-
 // Create Individual Hours Nodes
-const individualHourNodes = () => {
+const individualHourNodesToday = () => {
   let parentDiv = document.getElementById("hourlyGraph");
-  for (let i = currentHour; i <= 23; i++) {
+  while (parentDiv.firstChild) {
+    parentDiv.removeChild(parentDiv.firstChild);
+  }
+  for (let i = todayHour; i <= 23; i++) {
+    let hourlyDiv = document.createElement("div");
+    hourlyDiv.setAttribute("id", "hour" + i);
+    hourlyDiv.setAttribute("class", "hourlyNode");
+    let AmOrPm = i >= 12 ? "PM" : "AM";
+    let hours = i % 12 || 12;
+    let finalTime = hours + " " + AmOrPm;
+    hourlyDiv.innerHTML = finalTime;
+
+    let iconDiv = document.createElement("img");
+    iconDiv.setAttribute("id", "hr" + i + "icon");
+
+    let tempDiv = document.createElement("div");
+    tempDiv.setAttribute("id", "hr" + i + "temp");
+
+    hourlyDiv.appendChild(iconDiv);
+    hourlyDiv.appendChild(tempDiv);
+    parentDiv.appendChild(hourlyDiv);
+  }
+};
+
+const individualHourNodesTomorrow = () => {
+  let parentDiv = document.getElementById("hourlyGraph");
+  while (parentDiv.firstChild) {
+    parentDiv.removeChild(parentDiv.firstChild);
+  }
+  for (let i = 0; i <= 23; i++) {
     let hourlyDiv = document.createElement("div");
     hourlyDiv.setAttribute("id", "hour" + i);
     hourlyDiv.setAttribute("class", "hourlyNode");
@@ -58,9 +66,13 @@ const individualHourNodes = () => {
 const createHourlyForecastFooter = () => {
   createNewDiv("hourlyGraph", "content");
 
-  individualHourNodes();
+  individualHourNodesToday();
 
   defaultHourlyData();
 };
 
-export { createHourlyForecastFooter, currentHour };
+export {
+  createHourlyForecastFooter,
+  individualHourNodesTomorrow,
+  individualHourNodesToday,
+};
